c6d0bb9141f95609e0d647f4d6ef8315e1e41436
Nitin-web-dev <nitinwebdev5@gmail.com>
2026-07-03 17:21:35 +0530

    added input feature and store in json file

--- Task Tracker/task-cli.js
+++ Task Tracker/task-cli.js
@@ -1,1 +1,23 @@
-console.log(process.argv); // to get input from cli 
+const fs = require("fs");
+
+const file = "tasks.json";
+
+let tasksData = [];
+
+console.log(tasksData)
+if (fs.existsSync(file)){
+    tasksData = JSON.parse(fs.readFileSync(file, "utf-8"));
+}
+console.log(tasksData)
+tasksData.push({
+    name : process.argv[2],
+    data : process.argv[3]
+    
+})
+console.log(tasksData)
+fs.writeFileSync(file, JSON.stringify(tasksData, null, 2))
+console.log('end')
+
+// console.log(process.argv); // to get input from cli 
+
+
