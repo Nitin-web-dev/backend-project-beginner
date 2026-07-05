5e46229c44f57991983675cd36fec6866888214e
Nitin-web-dev <nitinwebdev5@gmail.com>
2026-07-05 16:43:58 +0530

    added getting input from cli and save in json file

--- Task Tracker/task-cli.js
+++ Task Tracker/task-cli.js
@@ -3,7 +3,21 @@ const {loadTasks, saveTasks } = require("./fileHandler.js");
 let tasks = loadTasks(); // load the exist or ya jo pehle se exist ya pehle se json file me data save hai 
 
 // to do 
-// get input from cli
+// get input from cli by using process.argv
+const command = process.argv[2];
+const message = process.argv[3];
+
+console.log(command, message);
+const dataSkeleton = {
+    id : 1,
+    description: message,
+    Status: "todo",
+    createdAt : new Date(),
+    updateAt : "yet to do"
+}
+
+
+saveTasks(dataSkeleton);
 // add data into previous tasks
 // save the data 
 
