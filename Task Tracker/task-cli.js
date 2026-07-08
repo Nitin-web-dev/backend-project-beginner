e29575c0c2c344f518502f55953f64da6985fea5
Nitin-web-dev <nitinwebdev5@gmail.com>
2026-07-08 15:30:26 +0530

    there is a bug here solve it

--- Task Tracker/task-cli.js
+++ Task Tracker/task-cli.js
@@ -1,30 +1,27 @@
-const {loadTasks, saveTasks } = require("./fileHandler.js");
-const {AddTasks} = require("./taskManager.js");
-let tasks = loadTasks(); // load the exist or ya jo pehle se exist ya pehle se json file me data save hai 
+const { loadTasks, saveTasks } = require("./fileHandler.js");
+const { AddTasks } = require("./taskManager.js");
+let tasks = loadTasks(); // load the exist or ya jo pehle se exist ya pehle se json file me data save hai
 
-// to do 
 // get input from cli by using process.argv
 const command = process.argv[2];
 const message = process.argv[3];
 
-console.log(command, message);
-// the data in json file should store like this
-// const dataSkeleton = {
-//     id : 1,
-//     description: message,
-//     Status: "todo",
-//     createdAt : new Date(),
-//     updateAt : "yet to do"
-// }
-
-// Addtask is a function to add input data into a object like schema or skeleton and return it to save 
+// Addtask is a function to add input data into a object like schema or skeleton and return it to save
 const data = AddTasks(message);
-
 
 //  to do : add data into previous tasks
 
 
-// save the data 
+// save the data
 saveTasks(data);
 
-console.log(tasks) // to do : should output latest data not previous data
+
+
+    let currentID = loadTasks();
+    let id = currentID + 1;
+  console.log(currentID);
+console.log(typeof currentID);
+console.log(Array.isArray(currentID));
+
+console.log(tasks)
+console.log(data); // to do : should output latest data not previous data
--- Task Tracker/taskManager.js
+++ Task Tracker/taskManager.js
@@ -1,11 +1,16 @@
 /// identify which operation to perform add update read or delete in this file or all tasks logic
+const { loadTasks, saveTasks } = require("./fileHandler.js");
+
 
 function AddTasks (message)  {
+
+    let currentID = loadTasks();
+    let id = currentID.length + 1;
     return {
-    id :1,
+    id : id,
     description: message,
     Status : "todo",
-    createdAt : new Date(),
+    createdAt : new Date().toISOString,
     }
 }
 
