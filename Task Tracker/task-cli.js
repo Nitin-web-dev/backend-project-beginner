18f0b46ddd9a7183719b336beee65f6b9f10fa78
Nitin-web-dev <nitinwebdev5@gmail.com>
2026-07-09 18:27:53 +0530

    change some function and create some function

--- Task Tracker/task-cli.js
+++ Task Tracker/task-cli.js
@@ -1,27 +1,20 @@
-const { loadTasks, saveTasks } = require("./fileHandler.js");
-const { AddTasks } = require("./taskManager.js");
-let tasks = loadTasks(); // load the exist or ya jo pehle se exist ya pehle se json file me data save hai
+
+const { AddTasks,listAllTasks } = require("./taskManager.js");
+; // load the exist or ya jo pehle se exist ya pehle se json file me data save hai
 
 // get input from cli by using process.argv
 const command = process.argv[2];
 const message = process.argv[3];
 
-// Addtask is a function to add input data into a object like schema or skeleton and return it to save
-const data = AddTasks(message);
-
-//  to do : add data into previous tasks
-
-
-// save the data
-saveTasks(data);
+//todo: check for command which operation to perform;
+//create a switch for assign which function to call
 
 
 
-    let currentID = loadTasks();
-    let id = currentID + 1;
-  console.log(currentID);
-console.log(typeof currentID);
-console.log(Array.isArray(currentID));
 
-console.log(tasks)
-console.log(data); // to do : should output latest data not previous data
+// show all list and count them also
+const data = listAllTasks(); 
+
+console.log(data);
+
+
--- Task Tracker/taskManager.js
+++ Task Tracker/taskManager.js
@@ -1,20 +1,68 @@
 /// identify which operation to perform add update read or delete in this file or all tasks logic
+// quick reference for
+// # Adding a new task
+// task-cli add "Buy groceries"
+// # Output: Task added successfully (ID: 1)
+// # Updating and deleting tasks
+// task-cli update 1 "Buy groceries and cook dinner"
+// task-cli delete 1
+// # Marking a task as in progress or done
+// task-cli mark-in-progress 1
+// task-cli mark-done 1
+// # Listing all tasks
+// task-cli list
+// # Listing tasks by status
+// task-cli list done
+// task-cli list todo
+// task-cli list in-progress
+
 const { loadTasks, saveTasks } = require("./fileHandler.js");
 
-
-function AddTasks (message)  {
-
-    let currentID = loadTasks();
-    let id = currentID.length + 1;
-    return {
-    id : id,
+// todo : error in this function yet to solve it
+function AddTasks(message) {
+  const task = {
+    id: tasks.length + 1,
     description: message,
-    Status : "todo",
-    createdAt : new Date().toISOString,
-    }
+    status: "todo",
+    createdAt: new Date().toISOString(),
+    updatedAt: new Date().toISOString(),
+  };
+  // save the data
+  saveTasks(task);
+  return task;
 }
 
-
-module.exports  = {
-    AddTasks
+function updateTasksStutus(id, message) {
+  // todo
 }
+
+function deleteTask(id) {
+  // todo
+}
+
+function listAllTasks() {
+    const list = loadTasks();
+    let countList = list.length;
+  return { ['number of list']: countList,list};
+}
+
+function listAllTasksDone() {
+  // todo
+}
+
+function listAllTasksNotDone() {
+  // todo
+}
+function listAllTasksInProgess() {
+  // todo
+}
+
+module.exports = {
+  AddTasks,
+  updateTasksStutus,
+  deleteTask,
+  listAllTasks,
+  listAllTasksDone,
+  listAllTasksNotDone,
+  listAllTasksInProgess,
+};
